e95f240d409ffe20ace26819869ee230f28ad60c
matheuscoelho <matheuscoelhoo321@gmail.com>
2017-11-20 13:07:32 -0200

    asda
    asd

--- www/js/index.js
+++ www/js/index.js
@@ -73,6 +73,7 @@ var app = {
             receivedElement.setAttribute('style', 'display:block;');
 
             document.getElementById("teste").value = 'deu: ' + data.registrationId;
+
         });
 
         push.on('error', function(e) {
