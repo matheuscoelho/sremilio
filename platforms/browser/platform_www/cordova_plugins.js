001658b3d97e80ef16c6dbc040bb32cc3125192b
matheuscoelho <matheuscoelhoo321@gmail.com>
2017-11-22 18:27:04 -0200

    asd
    asd

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -1,18 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
-        "id": "cordova-sqlite-storage.SQLitePlugin",
-        "pluginId": "cordova-sqlite-storage",
-        "clobbers": [
-            "SQLitePlugin"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "cordova-sqlite-storage": "2.1.2"
-}
+{}
 // BOTTOM OF METADATA
 });
